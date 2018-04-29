4b3a189b46c7664a2796f212f667988e882ab4a5
Sergio Lucero <sergiolucero@gmail.com>
2018-04-29 04:44:44 +0000

    NEW DASH

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-app = Flask(__name__)
+app = dash.Dash()
 
-@app.route('/') 
-def hello_world(): 
-    return 'Hello, Larry!' 
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
